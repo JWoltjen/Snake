b00d75006a9de0c068be499480ad7ae7fa386d9c
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-07-02 13:58:23 -0500

    add controls to snake

--- main.js
+++ main.js
@@ -6,6 +6,80 @@ const k = kaboom({
     scale: 2, 
     clearColor: [0, 0, 0, 1]
 })
+
+function movement(){
+    const {
+        vec2
+    } = k
+    const direction = vec2(0, 0)
+    const speed = 1
+
+    return {
+        add() {
+            this.movement.right(); 
+        },
+        update(){
+            if(!this.pos)
+            {
+                console.error('missing pos component')
+                return
+            }
+            this.pos.x += direction.x * speed
+            this.pos.y += direction.y * speed
+        }, 
+        movement: {
+            left() {
+                direction.x = -1
+                direction.y = 0
+            },
+            right() {
+                direction.x = 1
+                direction.y = 0
+            },
+            up() {
+                direction.x = 0
+                direction.y = -1
+            }, 
+            down(){
+                direction.x = 0
+                direction.y = 1
+            }
+        }
+    }
+}
+
+function controls() {
+    const {
+        keyPress
+    } =k
+    return {
+        add() {
+            keyPress('left', () => {
+                if(!this.movement){
+                    console.error("missing movement componemnt")
+                    return
+                }
+
+                this.movement.left()
+            })
+
+            keyPress('right', () => {
+                this.movement.right()
+            })
+
+            keyPress('down', () => {
+                this.movement.down()
+            })
+
+            keyPress('up', () => {
+                this.movement.up()
+            })
+
+        }
+    }
+}
+
+
 function Snake() {
     const {
         add, 
@@ -19,7 +93,9 @@ function Snake() {
         pos(8, 8), 
         rect(16, 16),
         color(0, 1, 0),
-        origin('center')
+        origin('center'),
+        movement(), 
+        controls()
     ])
 }
 
