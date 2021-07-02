cbf3096fea40c29d798efaf458e09ab9236a574d
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-07-02 13:07:16 -0500

    display setup screen in live server

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-const k = kaboom();
+import kaboom from "https://kaboomjs.com/lib/0.5.1/kaboom.mjs";
+
+const k = kaboom({
+    width: 320, 
+    height: 240, 
+    scale: 2, 
+    clearColor: [0, 0, 0, 1]
+})
 
 k.scene('test', () => {
     console.log('test scene loaded')
